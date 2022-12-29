2397214a4e1e0910c4cd87f061f70ac0f8521070
steph bug <chronhubgit@gmail.com>
2022-12-29 09:23:43 +0100

    tests

---
+++ tests/Foo/FooClassTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chronhub\Core\Tests\Foo;
+
+use Chronhub\Core\Tests\UnitTest;
+
+final class FooClassTest extends UnitTest
+{
+    // @test
+    public function it_assert_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/UnitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chronhub\Core\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class UnitTest extends TestCase
+{
+
+}
